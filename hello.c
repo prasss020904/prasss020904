e650a6ce5c750a369057efd4658c9b9763fa8c75
71230987 <prastha0209@gmail.com>
2023-09-28 22:44:27 +0700

    71230987

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include<studio.h>
+
+void main(void) {                                                         
+char* name;
+
+printf("what is your name");
+scanf("%s , name")                                                               
+printf("hello, %s , name");
+}
